32cdd2df102466fc3cca562381427a07e17d2b28
Hammad <hammad2091504@hotmail.com>
2022-09-14 13:16:41 +0000

    Add view.py, class PostLike(View) > post_details.html > url

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('summernot/', include('django_summernote.urls')), 
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
+    
 ]
